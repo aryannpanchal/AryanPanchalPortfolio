f7e0158cfc941b7733e7a641669ffff6afb17044
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2025-06-17 12:36:07 +0530

    update hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,7 +6,7 @@ import { config } from "../../constants/config";
 
 const Hero = () => {
   return (
-    <section className={`relative mx-auto h-screen w-full mt-12`}>
+    <section className={`relative mx-auto h-[320px] w-full mt-12`}>
       <div
         className={`absolute inset-0 top-[120px] mx-auto max-w-7xl ${styles.paddingX} flex flex-row items-start gap-5`}
       >
@@ -26,7 +26,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* <ComputersCanvas />
 
       <div className="xs:bottom-10 absolute bottom-32 flex w-full items-center justify-center">
         <a href="#about">
@@ -44,7 +44,7 @@ const Hero = () => {
             />
           </div>
         </a>
-      </div>
+      </div> */}
     </section>
   );
 };
